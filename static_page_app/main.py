7a29ee184dec14a5c1a45efe18a4bf16956fab60
hashihei <ma967gachapin@gmail.com>
2019-12-20 23:40:08 +0900

    1

---
+++ static_page_app/main.py
@@ -0,0 +1,43 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START gae_python37_app]
+from flask import Flask, render_template, request
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+def static_main_page():
+    return render_template("index.html")
+
+@app.route('/article1', methods=['GET', 'POST'])
+def static_article_page():
+    if request.method == 'GET':
+        input_value = request.args.get('get_value')
+    elif request.method == 'POST':
+        input_value = request.form['input1']
+
+    return render_template("article1.html", output_value=input_value)
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]

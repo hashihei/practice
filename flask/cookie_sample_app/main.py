0bd884e7a5b111750dfc513f384208b44ef21581
hashihei <ma967gachapin@gmail.com>
2019-12-30 15:40:03 +0900

    20191230_05

--- flask/cookie_sample_app/main.py
+++ flask/cookie_sample_app/main.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START gae_python37_app]
 from flask import Flask, render_template, request , make_response
 from datetime import datetime
 

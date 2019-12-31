0ef87ae41843c8dae34ea3a0a7b665bd64b66e69
hashihei <ma967gachapin@gmail.com>
2019-12-31 17:31:15 +0900

    201912311731

---
+++ firestore/snippets.py
@@ -0,0 +1,30 @@
+from google.cloud import firestore
+
+# Project ID is determined by the GCLOUD_PROJECT environment variable
+# create firestore client
+db = firestore.Client()
+
+#Add data 1
+doc_ref = db.collection(u'users').document(u'alovelace')
+doc_ref.set({
+    u'first': u'Ada',
+    u'last': u'Lovelace',
+    u'born': 1815
+})
+
+#Add data 2
+doc_ref = db.collection(u'users').document(u'aturing')
+doc_ref.set({
+    u'first': u'Alan',
+    u'middle': u'Mathison',
+    u'last': u'Turing',
+    u'born': 1912
+})
+
+#read data from collection
+# but data exist local.
+users_ref = db.collection(u'users')
+docs = users_ref.stream()
+
+for doc in docs:
+    print(u'{} => {}'.format(doc.id, doc.to_dict()))
